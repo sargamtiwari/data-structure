9073b8149b0c47c2c75f091d33aa227717f9af54
sargamtiwari <43930217+sargamtiwari@users.noreply.github.com>
2019-04-05 15:34:07 +0530

    Update a.py

--- a.py
+++ a.py
@@ -10,7 +10,6 @@ from flask import Flask, jsonify
 app=Flask(__name__)
 @app.route("/")
 def hello():
-    pridiction=[123.566,155.122,678.89]
-    return jsonify({pridiction})
+    return "hi this is me"
 if __name__=='__main__' :
     app.run()
